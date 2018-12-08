65cb6ba3796d7389440c1d6c1e4cc78474f58948
barbaradecares <barbaradecares@hotmail.com>
2018-12-08 01:47:32 -0600

    done

--- src/FordQuoteComponent.js
+++ src/FordQuoteComponent.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-export default class FordQuoteComponent extends Component {
+class FordQuoteComponent extends Component {
   render() {
     return (
       <div className="bar" id="ford">
-        <i><p>&quot;If I had asked people what they wanted, they would have said faster horses&quot;</p></i>
+        <i>
+          <p>
+            &quot;If I had asked people what they wanted, they would have said
+            faster horses&quot;
+          </p>
+        </i>
         <br />
         &#9;-Henry Ford
       </div>
     );
   }
 }
+export default FordQuoteComponent;
